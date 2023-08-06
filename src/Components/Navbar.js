0d94066840eb64b77213ea0a76f848a9e1c99dc1
Avanikant <exoduspatronum@gmail.com>
2023-08-06 11:18:13 +0530

    Added Course Project Number

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,9 @@
 import logo from "../images/airbnb-logo.png";
 export default function Navbar() {
   return (
-    <nav>
+    <nav className="navbar">
       <img src={logo} alt="Logo_airbnb" />
+      <h1>React Course: Project 2!</h1>
     </nav>
   );
 }
